7e86f91c76a4c52087f0a01dc56ca7580eb661d0
comee-system <system@comee.jp>
2021-06-18 11:42:48 +0900

    error

--- myapp/src/Controller/GraphsController.php
+++ myapp/src/Controller/GraphsController.php
@@ -1102,6 +1102,7 @@ class GraphsController extends AppController
     }
 
     public function tableDataExport($graphe_id="",$sa=""){
+        error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
         $alphabet = range('A', 'Z');
         // 入出力の情報設定
         $driPath    = realpath(TMP) . "/excel/";
